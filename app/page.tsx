69cba7010f8d10bcc997c73c0a567b3568073cb0
Elena Korchuganova <korchel21@gmail.com>
2024-02-17 20:27:47 +0300

    basic config

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const a = 11;
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +16,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{a}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
