47e8d948939411101f895360d5b050a5b5795289
Elena Korchuganova <korchel21@gmail.com>
2024-02-18 17:58:36 +0300

    buttons and headings added

---
+++ components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import { ButtonProps } from "./Button.props";
+import styles from './Button.module.css';
+import cn from 'classnames';
+import ArrowIcon from './arrow.svg';
+
+export const Button = ({ variant, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={cn(styles.button, className, {
+        [styles.primary]: variant === 'primary',
+        [styles.ghost]: variant === 'ghost'
+      })}
+      {...props}
+    >
+      {children}
+      {arrow !== 'none' &&
+        <span
+          className={cn(styles.arrow, {[styles.down]: arrow === 'down'})}
+        >
+          <ArrowIcon/>
+        </span>
+      }
+    </button>
+  );
+};
